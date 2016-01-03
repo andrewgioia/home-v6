7ae18c0013e467ba197c77adc5237e767c0edf24
Andrew Gioia <andrewgioia@gmail.com>
2016-01-03 12:04:44 -0500

    Updating FontAwesome; media queries are almost done for small browsers and phones

--- index.php
+++ index.php
@@ -48,20 +48,29 @@
                     </h1>
                     <menu class="navigation">
                         <div class="inner">
-                            <a href="#me" class="item self">Biography</a>
-                            <a href="#pj" class="item self">Projects</a>
-                            <a href="#em" class="item self">Contact Me</a>
+                            <a href="#me" class="item self">
+                                <span class="default">Biography</span>
+                                <i class="fa fa-info-circle narrow"></i>
+                            </a>
+                            <a href="#pj" class="item self">
+                                <span class="default">Projects</span>
+                                <i class="fa fa-database narrow"></i>
+                            </a>
+                            <a href="#em" class="item self">
+                                <span class="default">Contact Me</span>
+                                <i class="fa fa-envelope narrow"></i>
+                            </a>
                             <a href="#hi" class="item self top-1">
                                 <i class="fa fa-rocket"></i>
                             </a>
                         </div>
                     </menu>
                     <aside class="relative-position top-1">
-                        <a href="javascript:;" id="open" data-state="closed" class="white-text">
+                        <a href="javascript:;" id="open" class="white-text closed">
                             <i class="fa fa-navicon"></i>
                         </a>
                     </aside>
-                    <aside>
+                    <aside class="hide-768">
                         <a href="mailto:hi@andrewgioia.com">hi@andrewgioia.com</a>
                     </aside>
                 </div>
@@ -117,16 +126,16 @@
         <article id="me">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-xs-10 col-xs-offset-1
+                    <div class="col-xs-12 col-xs-offset-0
                                 col-sm-7 col-sm-offset-1">
                         <h1 class="no-margin">
-                            A Brief History.
+                            Background
                         </h1>
                         <h2 class="top-margin-20 bottom-padding-20">
-                            Just a bit about me and what I do.
+                            Hi, my name is Andrew Gioia.
                         </h2>
                         <p>
-                            A few years ago I started an educational technology company called <a href="http://teachboost.com">TeachBoost</a> with my brother, <a href="http://mikegioia.com">Mike</a>, and friend, <a href="http://jason.deroner.com">Jason</a>. We make web-based productivity and reporting tools for K-12 schools so that they can leverage their professional development into something wonderful for their educators and students. Day to day I now manage our customer engagement, maintain the brand and front-end design of the app, and run the operations-side of things.
+                            A few years ago I started an educational technology company called <a href="http://teachboost.com">TeachBoost</a> with my brother, <a href="http://mikegioia.com">Mike</a>, and friend, <a href="http://jason.deroner.com">Jason</a>. We make web-based development and reporting tools for K-12 schools so that they can leverage their professional development into something wonderful for their educators and students. Day to day I work remotely in Philadelphia to manage our customer engagement, maintain the brand/design of the app, and run the operations-side of things.
                         </p>
                         <p>
                             Prior to TeachBoost I was an attorney for two years at a <a href="http://rubinfortunato.com">law firm</a> in Philadelphia. I graduated from the University of Michigan Law School in 2010 and Cornell University in 2007. I grew up outside Philadelphia and currently love living near the art museum.
@@ -156,12 +165,10 @@
         <article id="pj">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1
-                                centered-text">
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1 centered-text">
                         <h1>Projects and Other Work</h1>
                     </div>
-                    <div class="col-sm-8 col-sm-offset-2
-                                centered-text top-padding-10">
+                    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 centered-text top-padding-10">
                         <h2>Apps and Software Projects</h2>
                         <ol class="projects left-justified">
                             <li>
@@ -466,14 +473,8 @@
     <!-- Hamburger menu -->
     <script>
         $( '#open' ).click( function() {
-            var state = $( this ).data( 'state' );
-            if ( state == 'closed' ) {
-                $( 'menu' ).animate( { width: 'toggle' }, 500 );
-                $( this ).data( 'state', 'open' );
-            } else {
-                $( 'menu' ).animate( { width: 'toggle' }, 500 );
-                $( this ).data( 'state', 'closed' );
-            }
+            $( 'menu' ).animate( { width: 'toggle' }, 500 );
+            $( this ).toggleClass( 'closed open' );
         });
     </script>
 
